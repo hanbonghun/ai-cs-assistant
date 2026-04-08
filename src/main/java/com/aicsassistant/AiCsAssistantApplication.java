34ca5b47dbdbf394368b66269541fa67d4907533
hanbonghun <bng4535@gmail.com>
2026-04-08 15:55:48 +0900

    chore: 스프링 부트스트랩

---
+++ src/main/java/com/aicsassistant/AiCsAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.aicsassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCsAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCsAssistantApplication.class, args);
+    }
+}
---
+++ src/test/java/com/aicsassistant/AiCsAssistantApplicationTests.java
@@ -0,0 +1,14 @@
+package com.aicsassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
+class AiCsAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
